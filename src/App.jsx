94736853d86e700cc3e46777d2812671fb55c94d
Stroller15 <shubhamverma6351@gmail.com>
2023-11-10 11:25:56 +0530

    initial commit with firebase

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  const putData = () => {
+
+  }
+
+  return (
+    <>
+      <h1>This is firebase learning</h1>
+      <button onClick={putData}>Put data in db</button>
+    </>
+  )
+}
+
+export default App
